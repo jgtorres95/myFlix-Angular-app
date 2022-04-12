8982a794065b22c2e936a4b45385a83def9059a7
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-04-12 00:05:45 -0700

    Create edit-profile-form.component.ts

---
+++ src/app/edit-profile-form/edit-profile-form.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-edit-profile-form',
+  templateUrl: './edit-profile-form.component.html',
+  styleUrls: ['./edit-profile-form.component.scss']
+})
+export class EditProfileFormComponent implements OnInit {
+
+  user: any = {};
+
+  @Input() userData = {
+    Username: this.user.Username,
+    Password: this.user.Password,
+    Email: this.user.Email,
+    Birthday: this.user.Birthday,
+  }
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<EditProfileFormComponent>,
+    public snackBar: MatSnackBar,
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getUser().subscribe((response: any) => {
+      this.user = response;
+      console.log(response);
+      return this.user
+    });
+  }
+
+  editUserProfile(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      localStorage.setItem('user', response.Username);
+      this.snackBar.open('Your profile was updated successfully.', 'OK', {
+        duration: 2000
+      });
+      setTimeout(() => {
+        window.location.reload();
+      });
+    });
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+}
